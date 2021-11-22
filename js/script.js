3397317cb70df1351c71ba2eb7ba2d3d664e5ba3
Timofey <tim02timka@mail.ru>
2021-11-22 17:50:21 +0700

    half verstka

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 window.addEventListener('DOMContentLoaded', ()=>{
+    // Появления шаров
+
+    const firstscreen__shar = document.querySelectorAll('.firstscreen__shar');
+    setTimeout(()=>{
+        firstscreen__shar.forEach((el,index) => {
+            setTimeout(()=>{
+                el.classList.remove('firstscreen__sharAnimate1');
+                el.classList.remove('firstscreen__sharAnimate2');
+            }, 150*index);
+        });
+    }, 350)
     
 })
